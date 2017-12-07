357c64e1f011abc0d8373141340644bc7e109746
Choy <alexanderchoy@multiplica.com>
2017-12-07 12:58:46 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
 const path = require('path');
 
 module.exports = {
+  // indica el contexto desde donde se ejecutara 'webpack' que es el root del proyecto
   context: __dirname,
   entry: './js/ClientApp.jsx',
   devtool: 'cheap-eval-source-map',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+  },
+  // el directorio desde donde servira el webpack-dev-server
+  devServer: {
+    publicPath: '/public/',
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json'],
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: true
+    chunks: true,
   },
+  // el enforce: pre indica que esta regla se ejecutara primero
   module: {
     rules: [
       {
         enforce: 'pre',
         test: /\.jsx?$/,
         loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
